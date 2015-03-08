8e86bb26511a0d374378fc09e2757a44d143c1f5
An An Yu <yuxx0535@umn.edu>
2015-03-07 22:28:46 -0600

    first commit

--- HW/HW3/detect_edges.m
+++ HW/HW3/detect_edges.m
@@ -22,13 +22,13 @@ function edge_image = detect_edges(input_image, hsize, sigma, Thigh, Tlow)
     M = 236 - 4 + 1;
     % strength and direction of each gradient
     G = sqrt(GradientX.^2 + GradientY.^2);     
-    theta = atan2(abs(GradientY),abs(GradientX));           % use atan?
+    theta = atan2(GradientY,GradientX);           % use atan?
     figure;imshow(uint8(G));
     max(max(theta))
     % round directions into 4 angles
     k = pi/4;   % (3pi/4 )/ 3
-    roundedTheta = round(theta./k)*k;
-    
+    roundedTheta = mod( round(theta./k), 4);
+   
     dir0 = zeros(N,M);
     dir45 = zeros(N,M);
     dir90 = zeros(N,M);
@@ -36,25 +36,22 @@ function edge_image = detect_edges(input_image, hsize, sigma, Thigh, Tlow)
     
     % indicator matrices
     dir0(roundedTheta == 0) = 1;        % indicated locations that are 0 degrees
-    dir45(roundedTheta == k) = 1;
-    dir90(roundedTheta == 2*k) = 1;
-    dir135(roundedTheta == 3*k) = 1;    % dir0 + dir45 + dir90 + dir135 = ones(N,M)
+    dir45(roundedTheta == 1) = 1;
+    dir90(roundedTheta == 2) = 1;
+    dir135(roundedTheta == 3) = 1;    % dir0 + dir45 + dir90 + dir135 = ones(N,M)
     
     % non-maximum suppression : sharpen edges
     Gout = zeros(N,M);
     % direction : 90 degrees
     G90 = G.*dir90;
-    figure;imshow(uint8(G90));
+    %figure;imshow(uint8(G90));
     G0 = G.*dir0;
-    figure;imshow(uint8(G0));
+    %figure;imshow(uint8(G0));
     G45 = G.*dir45;
-    figure;imshow(uint8(G45));
+    %figure;imshow(uint8(G45));
     G135 = G.*dir135;
-    max(max(G135))
-    figure;imshow(uint8(G135));
-    %roundedTheta(1:5,1:5)
-    %G(1:5,1:5)
-    %G0(1:5,1:5)
+    %figure;imshow(uint8(G135));
+    
     for x = 1 : N           % for each column
         for y = 1 : M
             if (x + 1 <= N) && (x-1 >= 1) && (G90(x,y) > G90(x+1, y)) && (G90(x,y) > G90(x-1, y))
@@ -62,7 +59,6 @@ function edge_image = detect_edges(input_image, hsize, sigma, Thigh, Tlow)
             end
         end
     end
-    max_in_row = max(G0');     % take row-wise maximum
     for x = 1 : N           % for each column
         for y = 1 : M
             if (y + 1 <= M) && (y-1 >= 1) && (G0(x,y) > G0(x, y+1)) && (G0(x,y) > G0(x, y-1))
@@ -71,53 +67,80 @@ function edge_image = detect_edges(input_image, hsize, sigma, Thigh, Tlow)
         end
     end
    
-    % direction : 45 degrees
-    % central diagonal
-    for x = 1 : N
-        if (x + 1 <= N) && (x-1 >= 1) && (G45(x,x) > G45(x+1, x+1)) && (G45(x,x) > G45(x-1, y-1))
-            Gout(x,x) = G45(x,x);
-        end
-    end
-    % lower left submatrix
-    for x = 2 : N-1         % index of top left corner 
-        %m = max(diag(G45(x : N, 1 : M - x + 1)));
-        % traverse the diagonal of the matrix
-        j = 1;
-        for i = x : N
-            if (x + 1 <= N) && (x-1 >= 1) && (j+1 <= M) && (j-1 >=1) && (G45(x,j) > G45(x+1, j+1)) && (G45(x,j) > G45(x-1, j-1))
-                Gout(x,j) = G45(x,j);
+     % direction : G45 degrees
+    for x = 1 : N           % for each column
+        for y = 1 : M
+            if (y + 1 <= M) && (y-1 >= 1) && (x + 1 <= N) && (x-1 >= 1) && (G45(x,y) > G45(x-1, y-1)) && (G45(x,y) > G45(x+1, y+1))
+                Gout(x,y) = G45(x,y);
             end
-        j = j + 1;    
         end
     end
-    % upper right submatrix
-    for y = 2 : M-1 
-        %m = max(diag(G45(1 : N - y + 1, y : M)));
-        % traverse the diagonal of the matrix
-        i = 1;
-        for j = y : M
-            if (y + 1 <= M) && (y-1 >= 1) && (i+1 <= N) && (i-1 >=1) && (G45(i,y) > G45(i+1, y+1)) && (G45(i,y) > G45(i-1, y-1))
-                Gout(i,y) = G45(i,y);
+    % direction : G135 degrees
+    for x = 1 : N           % for each column
+        for y = 1 : M
+            if (y + 1 <= M) && (y-1 >= 1) && (x + 1 <= N) && (x-1 >= 1) &&(G135(x,y) > G135(x+1, y-1)) && (G135(x,y) > G135(x-1, y+1))
+                Gout(x,y) = G135(x,y);
             end
-            i = i + 1;
         end
     end
-     figure;imshow(uint8(Gout));title('non-max');
-    % double thresholding
+    figure;imshow(uint8(Gout));title('non-max');
+     
+    % hysteresis
     highest = max(max(G))
     H = Thigh * highest;
     L = H * Tlow;
+    
+    % identify strong or weak pixels
+    strong = [];                        % 2 x k
     for x = 1 : N
         for y = 1 : M
             if Gout(x,y) >= H
-                Gout(x,y) = 255;
-            elseif L <= Gout(x,y)
-                Gout(x,y) = 128;
-            else
-                Gout(x,y) = 0;
+                strong = [strong, [x;y]];
             end
         end
     end
-     figure;imshow(uint8(Gout));title('double suppression');
-    figure;imshow(edge(input_image,'Canny'));title('Edge');
+    
+    % BFS on strong pixels
+    
+    frontier = [];
+    q_hd = 1;
+    q_tl = 1;
+    visited = zeros(N,M);
+    n_start = size(strong,2);
+    
+    Gh = zeros(N,M);
+    
+    for i = 1 : n_start
+        
+        frontier = [frontier, strong(:,i)];     % push root to Q
+        q_tl = q_tl + 1;                        
+        visited(strong(:,i)) = 1;               % mark discovered
+        Gh(strong(:,i)) = 255;                  
+        
+        while q_hd < q_tl
+            
+            % pop from Q
+            current = frontier(:,q_hd);         
+            q_hd = q_hd + 1;
+               
+            % for all of current's 8 neighbors
+            for i = -1 : 1
+                for j = -1 : 1
+                    x = current(1)+i;
+                    y = current(2)+j;
+                                    
+                    if x <= N && y<= M && x > 0 && y > 0 && (Gout(x , y) >= L) && (visited(x, y) == 0)
+                        Gh(x,y) = 255; 
+                        frontier = [frontier, [x;y]];   % push to frontier
+                        q_tl = q_tl + 1; 
+                        visited(x,y) = 1;               % label as discovered
+                    end
+                end
+            end
+        end
+    end
+    
+    figure;imshow(uint8(Gh));title('Hysteresis');
+    
+%    figure;imshow(edge(input_image,'Canny'));title('Edge');
 end
