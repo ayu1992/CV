5ebc5e69051d4063d7776ae9707579c7136721e2
An An Yu <yuxx0535@umn.edu>
2015-04-29 02:42:46 -0500

    operate in Lab space

--- Final_Project/code/iterative_noFlow.py
+++ Final_Project/code/iterative_noFlow.py
@@ -9,65 +9,31 @@ import pickle
 from util import thirteenNeighbors
 
 ###TUNE parameter
-hierarchy = 113				# pixel_range(0-1) * init_tau(0.02)*tau_rate(1.1)^n = max_chi_squared_dist(2) 
-tau = 0.02					# must be same as in readVideo.py
-tau_rate = 1.1 
+hierarchy = 113
+
 processedFrames = []
 # Load a bunch of results from first iteration
 rawFrames = pickle.load(open('rawFrames.p','rb'))
 colorMap = pickle.load(open('colormap.p','rb'))
-clusterId = pickle.load(open('EnforcedClusterId.p', 'rb'))	# pixel ((coordinates), frame) -> cluster id
-rawFlow = pickle.load(open('rawFlow.p', 'rb'))				# rawFlow[frameIdx] : (N, M, 2)
+clusterId = pickle.load(open('EnforcedClusterId.p', 'rb'))
+tau = 0.02		# same as in readVideo.py
 
 # get N, M
 img = rawFrames[0]
 [N, M, d] = img.shape  # dimensions
 
-# INITIALIZE LOOKUP MAPS
-# clusterNodes : cid -> list of pixels
+# initialize clusterNodes : cid -> list of pixels
 clusterNodes = {}
 # build clusterNodes 
-for pixel, clusterIndex in clusterId.iteritems():
+for node, clusterIndex in clusterId.iteritems():
 	if clusterIndex not in clusterNodes:
 		clusterNodes[clusterIndex] = []
-	clusterNodes[clusterIndex].append(pixel)
+	clusterNodes[clusterIndex].append(node)
 
 # build descriptors for histogram : cid -> histogram
-colorDescrpR = {}		
-flowDescrpR = {}
+descrpR = {}		
 
-print "build clusterFlow"
-# build clusterFlow : cid -> flow of cluster (numFrames x 16)
-clusterFlow = {}
-for pixel, clusterIndex in clusterId.iteritems():
-	if clusterIndex not in clusterFlow:
-		clusterFlow[clusterIndex] = np.zeros((len(rawFrames), 16)) + 10**-6
-	((x,y),f) = pixel
-	if f == len(rawFrames) - 1:
-		continue
-	dx = rawFlow[f][x,y][0]
-	dy = rawFlow[f][x, y][1]
-	angle = round(math.degrees(math.atan2(dy,dx))* 16 / 360) 
-	clusterFlow[clusterIndex][f][angle%16] += (math.sqrt(dx**2 + dy**2) )
-
-edges = []
-# HELPER FUNCTIONS :
-def thirteenNeighborsAlongFlow(((x,y),frameIndex),N,M,d):
-	# 8 neighbors in same frame
-	tmp = [(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)]
-	ret = [((s,t),frameIndex) for (s,t) in tmp if s >= 0 and s < N and t >= 0 and t < M]
-
-	# 9 neighbors along flow vector
-	frameIndex -= 1
-	if frameIndex >= 0:
-		dx = round(rawFlow[frameIndex][x, y][0])
-		dy = round(rawFlow[frameIndex][x, y][1]) 
-		tmp = [(x-1,y-1),(x-1, y),(x,y),(x-1, y+1),(x,y-1),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)]
-		tmp = map(lambda i,j : (i + dx, j + dy), tmp)
-		ret += [((s,t),frameIndex) for (s,t) in tmp if s >= 0 and s < N and t >= 0 and t < M]
-	return ret 
-
-# computes clusterNeighborMap[clusterId] -> a list of 26 neighbors
+# HELPER FUNCTIONS : 
 def findNeighbors():
 	for node, clusterIndex in clusterId.iteritems():
 		for neighborNode in thirteenNeighbors(node,N,M,len(rawFrames)):
@@ -76,53 +42,44 @@ def findNeighbors():
 				clusterNeighborMap[clusterIndex].add(neighborClusterIndex)
 				clusterNeighborMap[neighborClusterIndex].add(clusterIndex)
 
-# distance measure of two histograms
-def chiSquaredDistance(chist1, chist2, fhist1, fhist2):
-	dc = sum(map(lambda x,y: 0 if (x + y == 0) else (x-y)**2 / (x+y), chist1, chist2))/2		
-	df = sum(map(lambda x,y: 0 if (x + y == 0) else (x-y)**2 / (x+y), fhist1, fhist2))/2
-	dist = (1 - (1 - dc)*(1-df))**2
-	return dist#*math.sqrt(3)
+def chiSquaredDistance(hist1, hist2):
+	dist = sum(map(lambda x,y: 0 if (x + y == 0) else (x-y)**2 / (x+y), hist1, hist2))		
+	return dist/2.0
+#	dist = cv2.compareHist(hist1, hist2, cv2.cv.CV_COMP_CHISQR)
+# implement flow here
 
-# SEGMENTATION ALGORITHM : (Iterative)
+# SEGMENTATION ALGORITHM : 
+# Iterative
 skipCalStat = False
-
+edges = []
 for h in range(hierarchy):
 	print h
-
+	
 	if skipCalStat is False:
-		print "not skipping calculating stats"
-		#INITIALIZE STUFF
-		# internal variation for each cluster
+		# INITIALIZE STUFF
 		IntR = {cid : 0 for cid in clusterNodes}	# cid -> variation of a region
-		
 		# build adjacency map for clusters: cid -> set of neighbors
 		clusterNeighborMap = {cid: set() for cid in clusterNodes}
 		clusterSize = Counter([cid for cid in clusterNodes])
-
-		# Build RGB histogram for each cluster
-		#REPORT: use another color space? 
+		
+		# Build histogram and store descriptors for each cluster
 		print "build histograms"
 		for cid in clusterNodes:
+			# compute RGB histogram
 			# generate a list of RGB values
-			colorvalues = []
-			histvalues = []
+			values = []
 			for ((x,y),f) in clusterNodes[cid]:
-				colorvalues.append(rawFrames[f][x,y])
+				values.append(rawFrames[f][x,y])
 
 			# flatten histogram
-			colorHist = cv2.calcHist([np.array([colorvalues], dtype=np.float32)],[0,1,2],None,[20,20,20],[0,1, 0 ,1, 0 ,1])
-			colorHist1D = []
-
-			for a in colorHist:
+			hist = cv2.calcHist([np.array([values], dtype=np.float32)],[0,1,2],None,[20,20,20],[0,1,0,1,0,1])
+			hist1D = []
+			for a in hist:
 				for b in a:
 					for c in b:
-						colorHist1D.append(float(c)/len(colorvalues))
-			colorDescrpR[cid] = np.array(colorHist1D) 		# a python list of images
-			
-			s = np.sum(clusterFlow[cid])
-
-			flowDescrpR[cid] = clusterFlow[cid].flatten() / s 
-
+						hist1D.append(float(c)/len(values))
+			descrpR[cid] = np.array(hist1D) # a python list of images
+		
 		# build adjacency map
 		findNeighbors()
 
@@ -131,7 +88,7 @@ for h in range(hierarchy):
 		for cid in clusterNodes:	
 			neighborsCid = clusterNeighborMap[cid]
 			for ncid in neighborsCid:
-				edges.append([chiSquaredDistance(colorDescrpR[cid], colorDescrpR[ncid], flowDescrpR[cid], flowDescrpR[ncid]), cid, ncid])
+					edges.append([chiSquaredDistance(descrpR[cid], descrpR[ncid]), cid, ncid])
 
 		# sort edge weights
 		print "sort edges", len(edges)
@@ -140,12 +97,11 @@ for h in range(hierarchy):
 		print max(edges)
 		print min(edges)
 	
-	# Merge clusters 
+	# merge clusters 
 	print "start merging"
+	tau *= 1.1
 	merge_cnt = 0
-	tau *= tau_rate
-	print "tau ", tau
-
+	print tau
 	for i in range(len(edges)):
 		
 		(weight, cid1, cid2) = edges[i]
@@ -177,6 +133,7 @@ for h in range(hierarchy):
 					edges[j][1] = cid1
 				if edges[j][2] == cid2:
 					edges[j][2] = cid1
+
 			clusterNeighborMap[cid1] = clusterNeighborMap[cid1].union(clusterNeighborMap[cid2])
 			del clusterNeighborMap[cid2]
 
@@ -187,24 +144,22 @@ for h in range(hierarchy):
 
 	# Final: display processed results
 	for f in range(len(rawFrames)):
-		imageio.imwrite('lab_flow/h='+str(1+h)+'_'+str(f)+'.png',processedFrames[f])
+		imageio.imwrite('lab_noflow/h='+str(1+h)+'_'+str(f)+'.png',processedFrames[f])
 		if (0xFF & cv2.waitKey(5) == 27) or processedFrames[f].size == 0:
 			break
 	
 	print "merged ",merge_cnt," clusters"
-
 	if merge_cnt == 0 :
 		skipCalStat = True
 	else:	# reset stats
 		skipCalStat = False
 		edges = []
-		colorDescrpR = {}
-		flowDescrpR = {}
+		descrpR = {}
 		IntR = {}
 		clusterNeighborMap = {}
 		clusterSize = {}
 		processedFrames = []
-		rawFlow = []
+			
 
 # Dump intermediate results
-#pickle.dump(clusterId, open('IterClusterId_30.p', 'wb'))
+pickle.dump(clusterId, open('IterClusterId_30.p', 'wb'))
--- Final_Project/code/readVideo.py
+++ Final_Project/code/readVideo.py
@@ -1,13 +1,15 @@
 import cv2
 import imageio
 import numpy as np  
+from numpy import *
 import math
 import operator
 from collections import Counter
 import pickle
+import video
 import random
 from util import thirteenNeighbors
-
+rawInput = []
 rawFrames = []
 processedFrames = []
 edges = []
@@ -15,16 +17,51 @@ edges = []
 # read in video file
 cam = cv2.VideoCapture("doves_8.mp4")  
 i = 0
-while i < 16:
+while i < 8:
 	ret, img = cam.read()
 	i += 1
 	if (type(img) == type(None)):
 		break
-	#if i%2 == 0:
-	print i
-	rawFrames.append(img)
+	if i%2 == 0:
+		print i
+		rawInput.append(img)
+
+# RUN OPTICAL FLOW
+print "Run optical flow"
+## Generate flow map for all pixels, E.g.: rawFlow(x,y,f) -> a length 16 vector
+flow = []
+# compute flows for all frame pairs
+prev = rawInput[0]
+prevgray = cv2.cvtColor(prev, cv2.COLOR_RGB2GRAY)
+# if test image
+#prevgray = cv2.cvtColor(np.array(prev, dtype=np.uint8), cv2.COLOR_RGB2GRAY)
+nextframe = 1
+
+while nextframe <= len(rawInput) - 1:
+	# convert color images to grayscale
+	nextgray = cv2.cvtColor(rawInput[nextframe], cv2.COLOR_RGB2GRAY)
+	# if test image
+#	nextgray = cv2.cvtColor(np.array(rawInput[nextframe], dtype=np.uint8), cv2.COLOR_RGB2GRAY)
+	# compute flow between two frames
+	flow.append(cv2.calcOpticalFlowFarneback(prevgray, nextgray,0.5,1,3,15,3,5,1))
+	prevgray = nextgray
+	nextframe += 1
+
+# print flow[0].shape # (N, M, 2)
+# flow[f][x,y] : [dx dy]
+print "length of rawFlow : ", len(flow)
+pickle.dump(flow, open('rawFlow.p', 'wb'))
+flow = []
+
+# CONVERT TO LAB Color Space
+print "convert to Lab space"
+for f in rawInput:
+	lab = cv2.cvtColor(f, cv2.COLOR_RGB2LAB)
+	rawFrames.append(lab / 256.0)
+
 img = rawFrames[0]
-[N, M, d] = img.shape  # dimensions
+[N, M, d] = img.shape  # dimensions	
+
 # BUILD LOOKUPs
 # initialize clusterId : node ((coordinates), frame) -> cluster id
 clusterId = {}
@@ -45,7 +82,7 @@ print "0. done building clusters"
 # HELPER FUNCTIONS
 
 def pixelDistance(((x1,y1),f1),((x2,y2),f2)):
-	return math.sqrt((int(rawFrames[f1][x1,y1,0])-int(rawFrames[f2][x2,y2,0]))**2 + (int(rawFrames[f1][x1,y1,1])-int(rawFrames[f2][x2,y2,1]))**2 + (int(rawFrames[f1][x1,y1,2])-int(rawFrames[f2][x2,y2,2]))**2)/255.0
+	return math.sqrt(((rawFrames[f1][x1,y1,0])-(rawFrames[f2][x2,y2,0]))**2 + ((rawFrames[f1][x1,y1,1])-(rawFrames[f2][x2,y2,1]))**2 + ((rawFrames[f1][x1,y1,2])-(rawFrames[f2][x2,y2,2]))**2)
 
 def firstIter():
 	print "1. sort graph by weight\n"
@@ -55,12 +92,16 @@ def firstIter():
 			edges.append((pixelDistance(node,n), node, n))
 	print "sort\n"
 	edges.sort()
+	print max(edges)
+	print min(edges)
+	merge_cnt = 0
 	print "2. iter all edges\n"
 	for weight, n1, n2 in edges:
 		if clusterId[n1] == clusterId[n2]:		
 			continue
 		# if edge weight < both regions then merge
 		if weight <= (IntR[clusterId[n1]] + tau / clusterSize[clusterId[n1]]) and weight <= (IntR[clusterId[n2]] + tau / clusterSize[clusterId[n2]]):
+			merge_cnt += 1
 			# new R of compound region
 			IntR[clusterId[n1]] = max(IntR[clusterId[n1]],IntR[clusterId[n2]],weight)
 			del IntR[clusterId[n2]]
@@ -72,14 +113,14 @@ def firstIter():
 				if cid == toDelete:
 					clusterId[pos] = clusterId[n1]
 	print len(edges)
-
+	print "merged ",merge_cnt,"clusters"
 
 # SEGMENTATION ALGORITHM : oversegmentation and merging
 tau = 0.02
 
 # First iteration : over-segmentation
 firstIter()
-
+print len(clusterId)
 # Dump results from first iteration(deterministics)
 pickle.dump(clusterId, open('firstIterClusterId.p', 'wb'))
 pickle.dump(clusterSize, open('firstIterClusterSize.p','wb'))
